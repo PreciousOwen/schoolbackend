6a574dfdaa7196e63e9918792fb483c82e1e56db
Precious Owen <precioustwaya1@gmail.com>
2025-07-02 23:28:17 +0300

    Add superuser creation to entrypoint script if it doesn't exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,11 @@ set -e
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+# Create superuser if it doesn't exist
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='preciousowen').exists():
+    User.objects.create_superuser('preciousowen', 'precioustwaya1@gmail.com', '12345678')
+END
 exec "$@"
